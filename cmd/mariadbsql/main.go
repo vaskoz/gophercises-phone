8ea85817c0fed6d9804ab057ed573627efc0d306
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-05-29 09:58:25 -0600

    update main.go to use package variables for testing

--- cmd/mariadbsql/main.go
+++ cmd/mariadbsql/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
+	"io"
 	"log"
 	"os"
 	"time"
@@ -11,11 +11,19 @@ import (
 	"github.com/vaskoz/gophercises-phone"
 )
 
+var (
+	stdout io.Writer = os.Stdout
+	exit   func(int) = os.Exit
+)
+
 func main() {
 	dataSourceName := os.Getenv("MARIADBSQL_DATASOURCE")
+	logger := log.New(stdout, "mariadbsql: ", log.Lshortfile)
 	db, openerr := sql.Open("mysql", dataSourceName)
 	if openerr != nil {
-		log.Fatal(openerr)
+		logger.Println(openerr)
+		exit(1)
+		return
 	}
 	for backoff := 1; backoff < 20; backoff *= 2 {
 		if err := db.Ping(); err == nil {
@@ -25,16 +33,22 @@ func main() {
 	}
 	rows, err := db.Query("SELECT number FROM phone")
 	if err != nil {
-		log.Fatal(err)
+		logger.Println(err)
+		exit(1)
+		return
 	}
 	for rows.Next() {
 		var number string
 		if err := rows.Scan(&number); err != nil {
-			log.Fatal(err)
+			logger.Println(err)
+			exit(1)
+			return
 		}
-		fmt.Printf("%s: %s\n", number, phone.Normalize(number))
+		logger.Printf("%s: %s\n", number, phone.Normalize(number))
 	}
 	if err := rows.Err(); err != nil {
-		log.Fatal(err)
+		logger.Println(err)
+		exit(1)
+		return
 	}
 }
