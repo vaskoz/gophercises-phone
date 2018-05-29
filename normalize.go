af35706a385af3d6548582882fda8a4fc9b23037
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-05-28 19:09:59 -0600

    change regex to use shorthand digit character class

--- normalize.go
+++ normalize.go
@@ -2,7 +2,7 @@ package phone
 
 import "regexp"
 
-var re = regexp.MustCompile("[^0-9]")
+var re = regexp.MustCompile("\\D")
 
 // Normalize removes all non-digit characters.
 func Normalize(number string) string {
